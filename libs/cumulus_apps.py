16e62e08b2d70d819bdcfb7e3028cc27f032bbf2
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2024-12-12 14:40:10 +0100

    fix converter call

--- libs/cumulus_apps.py
+++ libs/cumulus_apps.py
@@ -192,8 +192,9 @@ def generate_script(job_id, job_dir, app_name, settings, host):
 	stdout = utils.get_final_stdout_path(job_id)
 	stderr = utils.get_final_stderr_path(job_id)
 	# convert the input files eventually
+	converter = config.get("converter.raw.to.mzml")
 	for file in get_all_files_to_convert_to_mzml(job_dir, app_name, settings):
-		cmd += f"mono '{config.get("converter.raw.to.mzml")}' -i {file}  1>> {stdout} 2>> {stderr}"
+		cmd += f"mono '{converter}' -i {file}  1>> {stdout} 2>> {stderr}"
 	# generate the command line based on the xml file and the given settings
 	cmd = get_command_line(app_name, job_dir, settings, host.cpu, output_dir)
 	# redirect the output to the log directory
