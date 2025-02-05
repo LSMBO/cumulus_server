6c7933974ce4f0890802ac992c8b2ae6196819e0
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2025-02-05 10:06:42 +0100

    fixed a type in a log

--- cumulus.py
+++ cumulus.py
@@ -75,7 +75,7 @@ def start():
 	# create a pending job, it will be started when the files are all available, return the job id
 	job_id, job_dir = db.create_job(request.form, utils.JOB_DIR)
 	utils.create_job_directory(job_dir, request.form)
-	logger.info(f"Create job ${job_id}")
+	logger.info(f"Create job {job_id}")
 	#return str(job_id)
 	return jsonify(job_id, job_dir)
 
